4e785b3c5d20115f1b1d84342a0677296708e348
Adriano Junior <mand458@hotmail.com>
2023-05-28 23:37:23 -0300

    Changes by arisa

--- src/main/java/tads/ufrn/provapw2/controller/FrutaController.java
+++ src/main/java/tads/ufrn/provapw2/controller/FrutaController.java
@@ -15,9 +15,11 @@ import java.util.Optional;
 @Controller
 public class FrutaController {
     FrutaService frutaService;
+
     public FrutaController(FrutaService frutaService) {
         this.frutaService = frutaService;
     }
+
     @RequestMapping(value = {"/", "/index", "/index.html"}, method = RequestMethod.GET)
     public String getIndex(Model model) {
 
@@ -28,7 +30,40 @@ public class FrutaController {
         //model.addCookie(cookie);
         return "index.html";
     }
+    @GetMapping("/cadastro")
+    public String getCadastrarPage(Model model) {
+        Fruta f = new Fruta();
+        model.addAttribute("fruta", f);
+        return "cadastrarPage.html";
+    }
+    @PostMapping("/salvar")
+    public String doSalvar(@ModelAttribute @Valid Fruta f, Errors errors){
+        if (errors.hasErrors()){
+            return "cadastrarPage";
+        }else{
+            frutaService.salvarFruta(f);
+            return "redirect:/index";
+        }
+    }
 
+    @GetMapping(value = "/deletar/{id}")
+    public String deletarFruta(@PathVariable long id){
+        frutaService.deletarFruta(id);
+        return "redirect:/index";
+    }
+    @GetMapping("/editar/{id}")
+    public String getEditarPage(@PathVariable(name = "id") Long id, Model model){
+
+        Optional<Fruta> f = frutaService.buscarFrutaPorId(id);
+
+        if (f.isPresent()){
+            model.addAttribute("fruta", f.get());
+        }else{
+            return "redirect:/index";
+        }
+
+        return "editarPage";
+    }
 
 
 }
